7b74bec630e2abb57a883daa98dd6db73db84df8
geovanna de oliveira costa 12a <geovannaoliveiracosta10@gmail.com>
2025-12-03 23:43:06 -0300

    teste 2

--- index.js
+++ index.js
@@ -46,27 +46,28 @@ app.post("/cadastrar", (request, response) => {
 
 //rota para o login
 
-app.post("/login", (request, reponse) =>{
-    const { email, password} = response.body.user
+app.post("/login", (request, response) => {
+    const { email, password } = request.body;
 
-    const selectCommand = "SELECT * FROM geovannacosta_02ta WHERE email = ?"
+    const selectCommand = "SELECT * FROM geovannacosta_02ta WHERE email = ?";
 
-    database.query(selectCommand, [email], (error, user) =>{
-        if (error){
-            console.log(error)
-            return
+    database.query(selectCommand, [email], (error, users) => {
+        if (error) {
+            console.log(error);
+            return response.status(500).json({ message: "Erro no servidor" });
         }
 
-        if(user.length === 0 || user[0].password !== password){
-            reponse.json({ message: "Usuários ou senha incorretos!"})
+        if (users.length === 0 || users[0].password !== password) {
+            return response.status(401).json({ message: "Usuário ou senha incorretos!" });
         }
 
         response.json({
-            id: user[0].id,
-            name: user[0].name
-        })
-    })
-})
+            id: users[0].id,
+            name: users[0].name
+        });
+    });
+});
+
 
 app.listen(port, () =>{
     console.log(`Servidor rodando na porta ${port}!`)
